30c4c5e78ccd1699288b64e29cebfc604c06ad15
Maciej Krüger <mkg20001@gmail.com>
2020-03-18 06:42:43 +0100

    da

--- src/libutil/util.cc
+++ src/libutil/util.cc
@@ -1308,6 +1308,20 @@ bool hasSuffix(std::string_view s, std::string_view suffix)
         && s.substr(s.size() - suffix.size()) == suffix;
 }
 
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
 
 std::string toLower(const std::string & s)
 {
